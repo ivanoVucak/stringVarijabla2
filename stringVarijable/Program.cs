16301a5de51bb27799e3035b92271b5def3d83db
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-11-08 09:24:39 +0100

    Add files via upload

---
+++ stringVarijable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringVarijable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3;
+
+            s3 = (s1.Substring(0, 8)) + (s2.Substring(7, s1.Length));
+            Console.WriteLine(s3);
+
+            Console.ReadKey();
+        }
+    }
+}
